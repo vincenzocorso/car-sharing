c84472053d3971036d2ad98084d96b667ab3702f
Vincenzo Corso <vincenzocorso99@gmail.com>
2021-08-14 14:29:24 +0200

    feat(rent-service): add service main setup

---
+++ rent-service/src/main/java/it/vincenzocorso/carsharing/rentservice/RentServiceApplication.java
@@ -0,0 +1,13 @@
+package it.vincenzocorso.carsharing.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentServiceApplication.class, args);
+	}
+
+}
