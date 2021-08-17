bda2064d0bc9cbac8fa555e82dbbc39c84acfabf
Vincenzo Corso <vincenzocorso99@gmail.com>
2021-08-17 17:41:00 +0200

    feat(rent-service-api): add RentRestApi contract

---
+++ rent-service-api/src/main/java/it/vincenzocorso/carsharing/rentservice/api/web/RentRestApi.java
@@ -0,0 +1,32 @@
+package it.vincenzocorso.carsharing.rentservice.api.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@RequestMapping("/rents")
+public interface RentRestApi {
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	List<RentResponse> getRents(
+			@RequestParam(name = "customerId", required = false) String customerId,
+			@RequestParam(name = "states", required = false)
+				List<@Pattern(regexp = "PENDING|REJECTED|ACCEPTED|STARTED|ENDED") String> states,
+			@RequestParam(name = "limit", required = false, defaultValue = "10")
+				@Min(value = 1, message = "The limit must be positive")
+				@Max(value = 200, message = "The limit must be at most 200") Integer limit,
+			@RequestParam(name = "offset", required = false, defaultValue = "0") Integer offset
+	);
+
+	@GetMapping("/{rentId}")
+	@ResponseStatus(HttpStatus.OK)
+	RentResponse getRent(@PathVariable(name = "rentId") String rentId);
+
+	@PostMapping
+	RentResponse createRent(@Valid @RequestBody CreateRentRequest request);
+}
