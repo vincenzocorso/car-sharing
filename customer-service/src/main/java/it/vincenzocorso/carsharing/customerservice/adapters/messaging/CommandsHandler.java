afb0ef620e99442ee7e9816be25065244f0da6b3
Vincenzo Corso <vincenzocorso99@gmail.com>
2023-02-18 15:39:08 +0100

    refactor(customer-service): changed command handler implementation

--- customer-service/src/main/java/it/vincenzocorso/carsharing/customerservice/adapters/messaging/CommandsHandler.java
+++ customer-service/src/main/java/it/vincenzocorso/carsharing/customerservice/adapters/messaging/CommandsHandler.java
@@ -2,6 +2,7 @@ package it.vincenzocorso.carsharing.customerservice.adapters.messaging;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
+import it.vincenzocorso.carsharing.common.exceptions.InternalServerException;
 import it.vincenzocorso.carsharing.common.messaging.commands.CommandHeaders;
 import it.vincenzocorso.carsharing.common.messaging.commands.CommandProducer;
 import it.vincenzocorso.carsharing.customerservice.domain.ports.in.RentVehicleUseCase;
@@ -22,40 +23,31 @@ public class CommandsHandler {
     CommandProducer commandProducer;
 
     @Incoming("customer-service-commands")
-    public CompletionStage<Void> consume(Message<String> message) {
+    public CompletionStage<Void> dispatchMessage(Message<String> message) throws Exception {
         var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
         String type = new String(metadata.getHeaders().lastHeader(CommandHeaders.TYPE).value());
+        log.info("Received message of type: " + type);
 
-        // print the headers and payload
-        System.out.println("Received message: " + message.getPayload());
-        System.out.println("Type: " + type);
-
-        try {
-            if("VERIFY_CUSTOMER_COMMAND".equals(type)) {
+        switch (type) {
+            case "VERIFY_CUSTOMER_COMMAND" -> {
                 VerifyCustomerCommand command = this.objectMapper.readValue(message.getPayload(), VerifyCustomerCommand.class);
-                this.handleVerifyCustomerCommand(message.withPayload(command));
-            } else {
-                throw new RuntimeException("Unknown command type: " + type);
+                this.processVerifyCustomerCommand(message.withPayload(command));
             }
-        } catch (Exception ex) {
-            log.error("There was an exception during the command handling: ", ex);
-            return message.nack(ex);
+            default -> throw new InternalServerException("Unknown command type: " + type);
         }
 
         return message.ack();
     }
 
-    void handleVerifyCustomerCommand(Message<VerifyCustomerCommand> message) {
+    private void processVerifyCustomerCommand(Message<VerifyCustomerCommand> message) {
         var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
         String responseChannel = new String(metadata.getHeaders().lastHeader(CommandHeaders.RESPONSE_CHANNEL).value());
         String messageId = new String(metadata.getHeaders().lastHeader(CommandHeaders.MESSAGE_ID).value());
-
-        // print the response channel and the message id
-        System.out.println("Response channel: " + responseChannel);
-        System.out.println("Message id: " + messageId);
-
         VerifyCustomerCommand command = message.getPayload();
+        log.info("Processing command: " + command);
+
         boolean canRent = this.rentVehicleUseCase.verifyCustomer(command.customerId());
+
         VerifyCustomerCommandReply reply = new VerifyCustomerCommandReply(canRent);
         this.commandProducer.publishReply(responseChannel, messageId, command.customerId(), reply);
     }
