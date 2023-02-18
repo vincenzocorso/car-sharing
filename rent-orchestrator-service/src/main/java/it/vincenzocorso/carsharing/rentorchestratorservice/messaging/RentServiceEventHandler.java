f1d2463800dc0f660f6889ee80d29095783309f3
Vincenzo Corso <vincenzocorso99@gmail.com>
2023-02-18 17:59:51 +0100

    refactor(rent-orchestrator-service): changed the events handler implementation

--- rent-orchestrator-service/src/main/java/it/vincenzocorso/carsharing/rentorchestratorservice/messaging/RentServiceEventHandler.java
+++ rent-orchestrator-service/src/main/java/it/vincenzocorso/carsharing/rentorchestratorservice/messaging/RentServiceEventHandler.java
@@ -1,5 +1,6 @@
 package it.vincenzocorso.carsharing.rentorchestratorservice.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import it.vincenzocorso.carsharing.common.messaging.events.EventHeaders;
 import it.vincenzocorso.carsharing.rentorchestratorservice.RentSagaOrchestrator;
@@ -16,19 +17,34 @@ import java.util.concurrent.CompletionStage;
 @Slf4j
 @AllArgsConstructor
 public class RentServiceEventHandler {
+	private final ObjectMapper objectMapper;
 	private final RentSagaOrchestrator rentSagaOrchestrator;
 
 	@Incoming("rent-service-events")
-	public CompletionStage<Void> consume(Message<RentCreatedEvent> message) {
+	public CompletionStage<Void> dispatchMessage(Message<String> message) throws Exception {
+		var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
+		String type = new String(metadata.getHeaders().lastHeader(EventHeaders.TYPE).value());
+		log.info("Received message of type: " + type);
+
+		switch (type) {
+			case "RENT_CREATED_EVENT" -> {
+				RentCreatedEvent rentCreatedEvent = this.objectMapper.readValue(message.getPayload(), RentCreatedEvent.class);
+				this.processRentCreatedEvent(message.withPayload(rentCreatedEvent));
+			}
+			default -> log.info("An event of type {} was ignored", type);
+		}
+
+		return message.ack();
+	}
+
+	private void processRentCreatedEvent(Message<RentCreatedEvent> message) {
 		var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
 		String rentId = new String(metadata.getHeaders().lastHeader(EventHeaders.AGGREGATE_ID).value());
 		RentCreatedEvent rentCreatedEvent = message.getPayload();
-		log.info("RentCreateEvent (rentId " + rentId + ") arrived: " + rentCreatedEvent.customerId() + " " + rentCreatedEvent.vehicleId());
+		log.info("Processing event: " + rentCreatedEvent);
 
 		CreateRentSagaState state = new CreateRentSagaState(rentId, rentCreatedEvent.customerId(), rentCreatedEvent.vehicleId());
 		this.rentSagaOrchestrator.startCreateRentSaga(state);
-
-		return message.ack();
 	}
 }
 
