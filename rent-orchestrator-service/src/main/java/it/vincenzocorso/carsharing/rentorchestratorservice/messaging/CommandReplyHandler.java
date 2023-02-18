c49f803194e45037f8bb2212e992034e52e8a2ff
Vincenzo Corso <vincenzocorso99@gmail.com>
2023-02-18 16:00:39 +0100

    refactor(rent-orchestrator-service): changed command reply handler implementation

--- rent-orchestrator-service/src/main/java/it/vincenzocorso/carsharing/rentorchestratorservice/messaging/CommandReplyHandler.java
+++ rent-orchestrator-service/src/main/java/it/vincenzocorso/carsharing/rentorchestratorservice/messaging/CommandReplyHandler.java
@@ -1,8 +1,10 @@
 package it.vincenzocorso.carsharing.rentorchestratorservice.messaging;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import io.temporal.client.WorkflowClient;
+import it.vincenzocorso.carsharing.common.exceptions.InternalServerException;
 import it.vincenzocorso.carsharing.common.messaging.commands.CommandReplyHeaders;
 import it.vincenzocorso.carsharing.rentorchestratorservice.sagas.CreateRentSagaWorkflow;
 import it.vincenzocorso.carsharing.rentorchestratorservice.sagas.WorkflowCorrelation;
@@ -24,17 +26,30 @@ public class CommandReplyHandler {
 
     @Incoming("rent-orchestrator-service-response-channel")
     @Transactional
-    public CompletionStage<Void> consume(Message<String> message) throws Exception {
+    public CompletionStage<Void> dispatchMessage(Message<String> message) throws Exception {
+        var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
+        String type = new String(metadata.getHeaders().lastHeader(CommandReplyHeaders.TYPE).value());
+        log.info("Received message of type: " + type);
+
+        switch (type) {
+            case "VERIFY_CUSTOMER_COMMAND_REPLY" -> {
+                VerifyCustomerCommandReply reply = this.objectMapper.readValue(message.getPayload(), VerifyCustomerCommandReply.class);
+                this.processVerifyCustomerCommandReply(message.withPayload(reply));
+            }
+            default -> throw new InternalServerException("Unknown command type: " + type);
+        }
+
+        return message.ack();
+    }
+
+    private void processVerifyCustomerCommandReply(Message<VerifyCustomerCommandReply> message) {
         var metadata = message.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
         String correlationId = new String(metadata.getHeaders().lastHeader(CommandReplyHeaders.CORRELATION_ID).value());
-
-        VerifyCustomerCommandReply reply = this.objectMapper.readValue(message.getPayload(), VerifyCustomerCommandReply.class);
-        log.info("VerifyCustomerCommandReply (canRent: {}, rejectReason: {})", reply.canRent(), reply.rejectReason());
+        VerifyCustomerCommandReply reply = message.getPayload();
+        log.info("Processing command reply: " + reply);
 
         String workflowId = WorkflowCorrelation.<WorkflowCorrelation>findById(correlationId).workflowId;
         this.workflowClient.newWorkflowStub(CreateRentSagaWorkflow.class, workflowId).handleVerifyCustomerResponse(reply.canRent(), reply.rejectReason());
-
-        return message.ack();
     }
 }
 
