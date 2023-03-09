4c32da8855075694166503325d04549bc0e7c3c3
Vincenzo Corso <vincenzocorso99@gmail.com>
2023-03-09 07:43:22 +0100

    chore: moved debezium connectors to a different folder

--- scripts/register-debezium-connectors.sh
+++ scripts/register-debezium-connectors.sh
@@ -1,2 +1,2 @@
-curl -i -X POST -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:8083/connectors -d @../rent-service/rent-service-debezium-connector.json
-curl -i -X POST -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:8083/connectors -d @../customer-service/customer-service-debezium-connector.json
+curl -i -X POST -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:8083/connectors -d @../debezium-connectors/postgres-debezium-connector.json
+curl -i -X POST -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:8083/connectors -d @../debezium-connectors/mongodb-debezium-connector.json
